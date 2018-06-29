077850239a37dd92a4970c5617d7a9c2d62b9cc8
sparcsJara <jara0701@gmail.com>
2018-06-29 15:17:44 +0900

    adding permissino is_authenticated

--- backends/backends/settings.py
+++ backends/backends/settings.py
@@ -121,6 +121,9 @@ AUTH_USER_MODEL = 'users.User'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
