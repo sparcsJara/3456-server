97911f3b3a08ca21a453857bbd884b69e03ec26e
sparcsJara <jara0701@gmail.com>
2018-06-29 15:29:18 +0900

    add filter of query param to spot viewset

--- backends/apps/universities/views.py
+++ backends/apps/universities/views.py
@@ -27,6 +27,16 @@ class SpotViewSet(viewsets.ModelViewSet):
     queryset = Spot.objects.filter(is_validated=True)
     serializer_class = SpotSerializer
 
+    def get_queryset(self):
+        """
+        Optionally restricts the returned purchases to a given user,
+        by filtering against a `username` query parameter in the URL.
+        """
+        queryset = Spot.objects.all(is_validated=True)
+        category = self.request.query_params.get('category', None)
+        if category is not None:
+            queryset = queryset.filter(spot__category=category)
+        return queryset
 
 class StoryViewset(viewsets.ModelViewSet):
     queryset = Story.objects.all()
