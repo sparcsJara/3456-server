cf224c75e9eb966369b384c074159b98b080db15
sparcsJara <jara0701@gmail.com>
2018-06-29 15:15:08 +0900

    adding basic auth

--- backends/backends/settings.py
+++ backends/backends/settings.py
@@ -120,5 +120,9 @@ AUTH_USER_MODEL = 'users.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    )
 }
